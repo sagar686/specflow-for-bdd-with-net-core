331270ea70faded18a83ed4404ccec53f7d16eab
Sagar Sangvekar <sag61086@gmail.com>
2022-06-06 22:41:49 +0530

    Simple calculator to demonstrate specflow BDD

---
+++ SpecFlowCalculator/SpecFlowCalculator.Specs/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,69 @@
+﻿using TechTalk.SpecFlow;
+using FluentAssertions;
+namespace SpecFlowCalculator.Specs.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+        private readonly Calculator _calculator = new Calculator();
+        private int _result;
+
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            // _scenarioContext.Pending();
+            _calculator.FirstNumber = number;
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            //_scenarioContext.Pending();
+
+            _calculator.SecondNumber = number;
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            //TODO: implement act (action) logic
+
+            // _scenarioContext.Pending();
+
+            _result = _calculator.Add();
+
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            //TODO: implement assert (verification) logic
+
+            //_scenarioContext.Pending();
+
+            _result.Should().Be(result);
+        }
+    }
+}
---
+++ SpecFlowCalculator/SpecFlowCalculator/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SpecFlowCalculator
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            //throw new NotImplementedException();
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
